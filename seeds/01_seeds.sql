70de5cb87e2db67e65e142e5f5e9ff73c5b80e3f
Darcy <darcy.lauder@shaw.ca>
2022-02-09 21:10:53 -0700

    01_seeds.sql added

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -8,7 +8,7 @@ VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmv
 ('Margaret Wong', 'makaylaweiss@icloud.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 ('Leroy Hart', 'jaycereynolds@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
-INSERT INTO properties ('owner_id' , 'title' , 'description' , 'thumbnail_photo_url' , 'cover_photo_url' , 'cost_per_night' , 'parking_spaces' , 'number_of_bathrooms' , 'number_of_bedrooms' , 'country' , 'street' , 'city' , 'province' , 'post_code' , 'active'),
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
 VALUES 
 (1 , 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 93061, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', 28142, true),
 (1 , 'Blank corner', 'description' , 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 83680, true),
@@ -19,20 +19,20 @@ VALUES
 (7 , 'Shine twenty', 'description', 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg', 13644, 1, 7, 8, 'Canada', '340 Dokto Park', 'Upfufa', 'Nova Scotia', 29045, true),
 (8 , 'Game fill', 'description', 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg', 23428, 5, 6, 4, 'Canada', '834 Buwmi Road', 'Rotunif', 'Newfoundland And Labrador', 58224, true);
 
-INSERT INTO reservations ('start_date', 'end_date', 'property_id', 'guest_id'),
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES
-(2018-09-11, 2018-09-26, 2, 3),
-(2019-01-04, 2019-02-01, 2, 2),
-(2021-10-01, 2021-10-14, 1, 4),
-(2014-10-21, 2014-10-21, 3, 5),
-(2016-07-17, 2016-08-01, 3, 4),
-(2018-05-01, 2018-05-27, 4, 8),
-(2022-10-04, 2022-10-23, 5, 1),
-(2015-09-13, 2015-09-30, 6, 8),
-(2023-05-27, 2023-05-28, 4, 2),
-(2023-04-23, 2023-05-02, 8, 1);
+('2018-09-11', '2018-09-26', 2, 3),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 1, 4),
+('2014-10-21', '2014-10-21', 3, 5),
+('2016-07-17', '2016-08-01', 3, 4),
+('2018-05-01', '2018-05-27', 4, 8),
+('2022-10-04', '2022-10-23', 5, 1),
+('2015-09-13', '2015-09-30', 6, 8),
+('2023-05-27', '2023-05-28', 4, 2),
+('2023-04-23', '2023-05-02', 8, 1);
 
-INSERT INTO property_reviews ('guest_id', 'property_id', 'reservation_id', 'rating', 'message'),
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES
 (2, 5, 10, 3, 'messages'),
 (1, 4, 1, 4, 'messages'),
